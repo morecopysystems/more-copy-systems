503265acfa1e29c2f2231f0059071947baff67b8
Neil Nicholas <neilnicholaskanat@gmail.com>
2024-08-08 05:42:55 +0530

    added-printers

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className=" w-full px-9  sticky top-0 bg-white shadow-md z-30">
+    <nav className=" w-full px-9  sticky top-0 bg-white shadow-md  py-2  z-30">
       <div className="flex items-center justify-between lg:justify-around ">
         <a href="/">
-          <Image src={logo} alt="logo" width={130} />
+          <Image src={logo} alt="logo" width={2880} height={1440} className="w-[120px]" />
         </a>
         <div className=" gap-8 hidden md:flex">
           <a href="/" className="text-black/80  hover:text-blue-500 ">
